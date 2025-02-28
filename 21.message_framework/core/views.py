19f61b677b35832fbbf4754c59d5edd6c05eca1d
Sandip <jaiswarsandeep00@gmail.com>
2025-02-28 18:25:23 +0530

    update

---
+++ 21.message_framework/core/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render,redirect
+from . models import MarvelModel
+from .forms import MarvelForm
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        mf= MarvelForm(request.POST)
+        if mf.is_valid():
+            mf.save()
+            messages.success(request,'Thank you for joining avengers !')
+            return redirect('/')
+    else:
+        mf=MarvelForm()
+        mm= MarvelModel.objects.all()
+    return render(request,'core/index.html',{'mm':mm,'mf':mf})
+
+def delete_avenger(request,id):
+    mm = MarvelModel.objects.get(pk=id)
+    mm.delete()
+    return redirect('/')
+
+def update_avenger(request,id):
+    if request.method == 'POST':
+        mm= MarvelModel.objects.get(pk=id)
+        mf= MarvelForm(request.POST,instance=mm)
+        if mf.is_valid():
+            mf.save()
+            return redirect('/')
+    else:
+        mm= MarvelModel.objects.get(pk=id)
+        mf=MarvelForm(instance=mm)
+    return render(request,'core/update.html',{'mf':mf})
